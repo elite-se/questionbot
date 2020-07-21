1a5c3f7969de1f3714374fbf0c6aed4ea9b266b7
Dominik Horn <dominik.horn@tum.de>
2020-07-21 17:28:03 +0200

    initial commit
    very basic implementation of question bot

---
+++ questionbot.swift
@@ -0,0 +1,133 @@
+import Cocoa
+
+struct Task {
+    let statement: String
+    let solution: String
+}
+
+struct Chapter {
+    let title: String
+    var tasks: [Task] = []
+}
+
+func parse(taskfile path: String) -> [Chapter] {
+    var chapters: [Chapter] = []
+    
+    var text = ""
+    do {
+        text = try String(contentsOfFile: path, encoding: .utf8)
+    } catch {
+        print(error)
+    }
+    
+    var currentChapter = Chapter(title: "Unassigned")
+    var currentStatement: String? = nil
+    var currentSolution: String? = nil
+    text.components(separatedBy: .newlines).forEach { line in
+        if line.starts(with: "# ") {
+            // New chapter
+            let title = String(line.suffix(from: line.index(line.startIndex, offsetBy: 2)))
+            
+            if currentChapter.tasks.count > 0 {
+                chapters.append(currentChapter)
+            }
+            
+            currentChapter = Chapter(title: title)
+        } else if line.starts(with: "## ") {
+            if let statement = currentStatement, let solution = currentSolution {
+                currentChapter.tasks.append(Task(statement: statement, solution: solution))
+            }
+            
+            currentStatement = String(line.suffix(from: line.index(line.startIndex, offsetBy: 3)))
+            currentSolution = nil
+        } else {
+            if currentSolution == nil {
+                if !line.trimmingCharacters(in: .whitespaces).isEmpty {
+                    currentSolution = line
+                }
+            } else {
+                currentSolution! += "\n" + line
+            }
+        }
+    }
+
+    if let statement = currentStatement, let solution = currentSolution {
+        currentChapter.tasks.append(Task(statement: statement, solution: solution))
+    }
+    if currentChapter.tasks.count > 0 {
+        chapters.append(currentChapter)
+    }
+    
+    return chapters
+}
+
+
+var taskfilePath: String? = nil
+for argument in CommandLine.arguments[1..<CommandLine.arguments.count] {
+    switch argument {
+    case "-h", "--help":
+        print(
+"""
+This tool expects a file containing \"Tasks\" in Markdown format.
+h1 Elements are interpreted as Chapters, h2 Elements as Questions
+and Text under h2 Elements as answers to said questions.
+
+Example task file:
+
+# Chapter 1: Introduction
+## What is the meaning of Life?
+
+42
+
+## Give 5 reasons to stop procrastinating
+* Better grades
+* Less halve-finished sideprojects
+
+...
+
+"""
+        )
+        exit(0)
+    default:
+        taskfilePath = argument
+    }
+}
+guard let taskfile = taskfilePath else {
+    print("Usage: ./\(CommandLine.arguments[0]) <path-to-markdown-tasks-file>")
+    exit(1)
+}
+
+let chapters = parse(taskfile: taskfile)
+
+// ========================
+//  COMMAND LINE INTERFACE
+// ========================
+
+print("Identified the following Chapters:")
+chapters.enumerated().forEach { (index, chapter) in
+    print("  [\(index)]: \(chapter.title) (\(chapter.tasks.count) tasks)")
+}
+
+var index = 0
+if (chapters.count > 1) {
+    print("Which chapter do you want to practice?", terminator: "")
+    var answer = readLine()
+    while answer == nil || Int(answer!) == nil || Int(answer!)! >= chapters.count || Int(answer!)! < 0 {
+        print("Invalid answer, please choose one of (\(chapters.enumerated().map({ (i, _) in "\(i)" }).joined(separator: ", "))): ", terminator: "")
+        answer = readLine()
+    }
+
+    index = Int(answer!)!
+}
+let selectedChapter = chapters[index]
+print("Selected chapter \(index): \(selectedChapter.title)")
+
+selectedChapter.tasks.shuffled().enumerated().forEach { (index, task) in
+    // Clear screen
+    print("", terminator: "\u{001B}[2J\u{001B}[0;01H")
+    print("Task \(index)/\(selectedChapter.tasks.count): \u{001B}[1m\(task.statement)\u{001B}[0m")
+    _ = readLine()
+    print("\u{001B}[3m\(task.solution)\u{001B}[0m")
+    _ = readLine()
+}
+
---
+++ tasks.md
@@ -0,0 +1,20 @@
+# Motivation und Themenüberblick
+
+## Was ist der Unterschied zwischen den Begriffen Safety und Security?
+Security bezeichnet den Schutz vor Bedrohungen durch absichtliche Eingriffe
+eines Angreifers von außen, e.g., Hacker, Social Engineering, Verletzung der
+Vertraulichkeit, Sabotage, ...
+
+Safety bezeichnet den Schutz vor Bedrohungen, die sich während des
+Systembetriebs ungewollt durch vorhergesehene Eingriffe, Störungen im System
+oder der Systemumgebung ergeben, e.g., Hardwareausfälle, Konstruktionsfehler,
+Naturkatastrophen, ...
+
+## Was versteht man unter Sicherheit?
+Sicherheit ist die Freiheit von unvertretbaren Risiken im störungsfreien
+Systembetrieb sowie während und nach dem Auftreten von Störungen. Freiheit
+bedeutet hier, dass ein Risiko sehr gering und kontrollierbar ist.
+
+## Was ist das Ziel der Sicherheitsanalyse?
+Identifikation von Gefährdungen und ihren Ursachen sowie die Beurteilung ihres
+Risikos.
